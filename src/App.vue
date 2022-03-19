9e7fbf336468ee8cc07b7645938e65d635488de7
JanNaklicki <janek.naklicki@gmail.com>
2022-03-19 21:02:08 +0100

    Basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <header class="header">
+    <div>strona główna</div>
+    <div>portfolio</div>
+    <div>o mnie</div>
+    <div>kontakt</div>
+  </header>
+  <main>
+    <video
+      autoplay
+      muted
+      loop
+      id="myVideo"
+      style="
+        width: 70%;
+        max-width: 70%;
+        height: auto;
+        margin-left: auto;
+        margin-right: auto;
+      "
+    >
+      <source src="./assets/background.mp4" type="video/mp4" />
+    </video>
+  </main>
+  <footer></footer>
 </template>
 
 <script>
@@ -8,4 +31,36 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #040007;
+}
+.header {
+  padding: 20px;
+  padding-top: 40px;
+  display: flex;
+  justify-content: flex-end;
+  font-size: 35px;
+  font-family: "Josefin Sans", sans-serif;
+  position: sticky;
+  z-index: 10;
+}
+.header div {
+  margin-right: 20px;
+  color: #2f05bc;
+}
+.header div:hover {
+  color: #db015c;
+}
+main {
+  height: 130vh;
+  overflow: hidden;
+  margin-top: -18%;
+}
+main video {
+  width: 100%;
+}
+</style>
