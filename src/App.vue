b8c803f99f8d41ddb969b29ece0c2b3bb5308d88
JanNaklicki <49452909+JanNaklicki@users.noreply.github.com>
2022-09-26 03:59:04 +0200

    Included new components

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div>
     <Header />
     <Hero />
-    <Gallery />
+    <div class="content">
+      <Gallery />
+      <Branding />
+      <LifeOfArt />
+      <About />
+      <Footer/>
+    </div>
   </div>
 </template>
 
@@ -10,10 +16,14 @@
 import Header from "./components/Header.vue";
 import Hero from "./components/Hero.vue";
 import Gallery from "./components/Gallery.vue";
+import Branding from "./components/Branding.vue";
+import LifeOfArt from "./components/LifeOfArt.vue";
+import About from "./components/About.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
-  components: { Header, Hero, Gallery },
+  components: { Header, Hero, Gallery, Branding, LifeOfArt, About, Footer },
 };
 </script>
 
@@ -21,10 +31,18 @@ export default {
 @import "~@/assets/scss/vendors/bootstrap-vue/index";
 html,
 body {
-  background-color: black!important;
+  scroll-behavior: smooth;
+  background-color: black !important;
   margin: 0;
   padding: 0;
   font-family: "Montserrat", sans-serif;
 }
-
+.content {
+  position: relative;
+  z-index: 90;
+  margin-top: 95vh;
+  @include media-breakpoint-down(md) {
+    margin-top: 94vh;
+  }
+}
 </style>
