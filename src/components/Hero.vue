9139a0176761aa8c70700b0f92f975dad53e5bc8
JanNaklicki <49452909+JanNaklicki@users.noreply.github.com>
2022-09-26 03:58:22 +0200

    Fixed hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,12 @@
 <template>
-  <div  class="hero p-0">
-    <img src="../assets/ilonka.png" alt="" />
+  <div class="hero p-0">
+    <b-container fluid class="p-0 hero__container">
+      <div class="hero__title">
+        <h1 class="h1">@GIZA<span style="color: #df0160">LOU</span></h1>
+        <h2 class="h6">JUST MAKIN' THINGS ARTSY</h2>
+      </div>
+      <img src="../assets/ilonka2.gif" alt="" />
+    </b-container>
   </div>
 </template>
 
@@ -10,20 +16,55 @@ export default {};
 
 <style lang="scss">
 .hero {
-  background-color: white;
+  width: 100vw;
   height: 101vh;
   overflow: hidden;
   position: fixed;
   top: 0;
+  background-color: #030007;
   z-index: 1;
   @include media-breakpoint-down(md) {
-    height: 70vh;
+    display: flex;
+    justify-content: center;
   }
-  width: 100vw;
   img {
-    width: 100%;
+    width: 80%;
     height: 100%;
     object-fit: cover;
+    position: fixed;
+    top: 0;
+    @include media-breakpoint-down(md) {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  &__container {
+    display: flex;
+    justify-content: center;
+  }
+  &__title {
+    color: white;
+    top: 65vh;
+    left: 10vw;
+    z-index: 3;
+    position: fixed;
+    h1 {
+      font-size: 4rem;
+    }
+    @include media-breakpoint-down(md) {
+      h1 {
+      font-size: 2.5rem;
+    }
+      top: 80vh;
+      left: auto;
+    }
+  }
+  &__arrow {
+    font-size: 10px;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    flex-direction: column;
   }
 }
 </style>
